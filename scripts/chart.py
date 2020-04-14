03c39796f8b61d123d1e67ef99d28e6d5f2080fa
Michael Ludwig <michael.david.ludwig@gmail.com>
2020-04-13 23:09:01 -0400

    get chart script to work

--- scripts/chart.py
+++ scripts/chart.py
@@ -4,33 +4,44 @@ import json
 import sys
 import os
 
+pandas.set_option("display.max_rows", 200)
+
 with open(sys.argv[1]) as f:
-  config = json.load(f)
+    config = json.load(f)
 
 for chart in config['charts']:
-    line_dfs = []
+    plot_df = None
+
     for line in chart['lines']:
         d = line['directory']
         sum_df = None
         dirs = os.listdir(d)
+        title = line['title']
+
         for fn in dirs:
             df = pandas.read_csv(
                 os.path.join(d, fn),
                 delimiter=r'\s+',
-                names=['type', 'value', 'timestamp']
+                names=['type', title, 'timestamp']
             )
             df = df[df['type'] == line['type']]
-            df['type'] = line['title']
+            df = df[['timestamp', title]]
             df = df.sort_values('timestamp', ignore_index=True)
             df['timestamp'] -= df['timestamp'][0]
             if sum_df is None:
                 sum_df = df
             else:
-                sum_df['value'] += df['value']
-        sum_df['value'] = sum_df['value'] / len(dirs)
-        line_dfs.append(sum_df)
-    plot_df = pandas.concat(line_dfs)
-    print(plot_df)
-    plot_df = plot_df.pivot(index='timestamp', columns='type', values='value')
-    plot_df.plot()
-    plt.show()
+                sum_df[title] += df[title]
+
+        sum_df[title] = sum_df[title] / len(dirs)
+
+        if plot_df is None:
+            plot_df = sum_df
+        else:
+            plot_df = plot_df.merge(sum_df, on='timestamp', how='outer')
+    
+    plot_df.set_index('timestamp', inplace=True)
+    plot_df.interpolate(inplace=True)
+    plot_df.plot.line()
+
+plt.show()
