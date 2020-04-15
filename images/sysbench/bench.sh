2632dff4f5188cd4b3f472bfdc77ef20799b1414
Michael Ludwig <michael.david.ludwig@gmail.com>
2020-04-14 23:10:02 -0400

    update sysbench commands

--- images/sysbench/bench.sh
+++ images/sysbench/bench.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # CPU
-sysbench --time=900 --threads=4 --cpu-max-prime=300000000 cpu run
+sysbench --threads=4 --cpu-max-prime=150000000 cpu run
 
 # Memory
-sysbench --time=600 --threads=4 --memory-total-size=40000G --memory-oper=read memory run
-sysbench --time=600 --threads=4 --memory-total-size=40000G --memory-oper=write memory run
+sysbench --time=600 --threads=4 --memory-total-size=40000G --memory-block-size=2M --memory-oper=read memory run
+sysbench --time=600 --threads=4 --memory-total-size=40000G --memory-block-size=2M --memory-oper=write memory run
 
 # Disk
 sysbench --file-total-size=32G fileio prepare
