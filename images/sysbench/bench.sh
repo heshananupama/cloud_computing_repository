5fcef0ec87265b00e9f9e8e00cb6571a6425dc2b
Michael Ludwig <michael.david.ludwig@gmail.com>
2020-04-15 23:23:50 -0400

    decrease file size

--- images/sysbench/bench.sh
+++ images/sysbench/bench.sh
@@ -8,8 +8,8 @@ sysbench --time=600 --threads=4 --memory-total-size=40000G --memory-block-size=2
 sysbench --time=600 --threads=4 --memory-total-size=40000G --memory-block-size=2M --memory-oper=write memory run
 
 # Disk
-sysbench --file-total-size=32G fileio prepare
-sysbench --time=300 --threads=4 --file-test-mode=seqrd --file-total-size=32G fileio run
-sysbench --time=300 --threads=4 --file-test-mode=seqwr --file-total-size=32G fileio run
-sysbench --time=300 --threads=4 --file-test-mode=rndrd --file-total-size=32G fileio run
-sysbench --time=300 --threads=4 --file-test-mode=rndwr --file-total-size=32G fileio run
+sysbench --file-total-size=12G fileio prepare
+sysbench --time=300 --threads=4 --file-test-mode=seqrd --file-total-size=12G fileio run
+sysbench --time=300 --threads=4 --file-test-mode=seqwr --file-total-size=12G fileio run
+sysbench --time=300 --threads=4 --file-test-mode=rndrd --file-total-size=12G fileio run
+sysbench --time=300 --threads=4 --file-test-mode=rndwr --file-total-size=12G fileio run
